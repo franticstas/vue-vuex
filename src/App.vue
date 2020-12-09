b841b758abcfe1273282d3d349c4adb4df9de1aa
stas <franticstas@gmail.com>
2020-12-09 22:11:21 +0300

    add top nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <template>
   <div id="app">
-    Hello
+    <mcv-topbar></mcv-topbar>
 
     <router-view></router-view>
   </div>
 </template>
+
+<script>
+import McvTopbar from '@/components/Topbar';
+
+export default {
+  name: 'McvApp',
+  components: {
+    McvTopbar
+  }
+};
+</script>
