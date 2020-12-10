fa8a9438d63966ee5f16c149c2a0ae6408207472
stas <franticstas@gmail.com>
2020-12-10 22:14:56 +0300

    add get user by token

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -3,6 +3,7 @@ import {setItem} from '@/helpers/persistanceStorage';
 
 const state = {
   isSubmitting: false,
+  isLoading: false,
   currentUser: null,
   validationErrors: null,
   isLoggedIn: null
@@ -15,12 +16,17 @@ export const mutationTypes = {
 
   loginStart: '[auth] loginStart',
   loginSuccess: '[auth] loginSuccess',
-  loginFailure: '[auth] loginFailure'
+  loginFailure: '[auth] loginFailure',
+
+  getCurrentUserStart: '[auth] getCurrentUserStart',
+  getCurrentUserSuccess: '[auth] getCurrentUserSuccess',
+  getCurrentUserFailure: '[auth] getCurrentUserFailure'
 };
 
 export const actionTypes = {
   register: '[auth] register',
-  login: '[auth] login'
+  login: '[auth] login',
+  getCurrentUser: '[auth] getCurrentUser'
 };
 
 export const getterTypes = {
@@ -67,6 +73,19 @@ const mutations = {
   [mutationTypes.loginFailure](state, payload) {
     state.isSubmitting = false;
     state.validationErrors = payload;
+  },
+  [mutationTypes.getCurrentUserStart](state) {
+    state.isLoading = true;
+  },
+  [mutationTypes.getCurrentUserSuccess](state, payload) {
+    state.isLoading = false;
+    state.currentUser = payload;
+    state.isLoggedIn = true;
+  },
+  [mutationTypes.getCurrentUserFailure](state) {
+    state.isLoading = false;
+    state.currentUser = null;
+    state.isLoggedIn = false;
   }
 };
 
@@ -104,6 +123,23 @@ const actions = {
             mutationTypes.loginFailure,
             result.response.data.errors
           );
+        });
+    });
+  },
+  [actionTypes.getCurrentUser](context) {
+    return new Promise(resolve => {
+      context.commit(mutationTypes.getCurrentUserStart);
+      authApi
+        .getCurrentUser()
+        .then(response => {
+          context.commit(
+            mutationTypes.getCurrentUserSuccess,
+            response.data.user
+          );
+          resolve(response.data.user);
+        })
+        .catch(() => {
+          context.commit(mutationTypes.getCurrentUserFailure);
         });
     });
   }
